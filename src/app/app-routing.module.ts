c271431c764fae8565906e87e6c1fe4f479c4a2b
Ergoni3 <78090250+Ergoni3@users.noreply.github.com>
2021-04-20 10:43:13 -0300

    import do modal e service pra página no .ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { list } from '../app/Models/list.class';
+import { ListService } from '../app/Service/list.service';
 
 const routes: Routes = [];
 
