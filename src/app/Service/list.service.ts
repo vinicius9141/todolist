394a85c1081ba5d9cc2fb264f3c719164cb6c827
Ergoni3 <78090250+Ergoni3@users.noreply.github.com>
2021-04-20 10:34:34 -0300

    Model e service para comunicar back-end

---
+++ src/app/Service/list.service.ts
@@ -0,0 +1,4 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { list }  from '../Models/list.class';
+import { Observable } from 'rxjs';
