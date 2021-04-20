00033429ec686269b975e490b0e465915fa22005
Ergoni3 <78090250+Ergoni3@users.noreply.github.com>
2021-04-20 10:24:33 -0300

    Base estrutural do Front-end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,18 @@
 import { Component } from '@angular/core';
 
+export interface PeriodicElement {
+  name: string;
+  position: string;
+  weight: string;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 'Desenvolver Back-end', name: 'Fazer o banco de dados do To-do list', weight: 'Vinicius', symbol: 'H'},
+  {position: 'Desenvolver Front-end', name: 'Fazer o design da página do To-do list', weight: 'Gabriel', symbol: 'He'},
+ 
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +20,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
 }
