6dcf47fb465cb79cd13fe410233c5fee46f7d5e1
Ergoni3 <78090250+Ergoni3@users.noreply.github.com>
2021-04-20 10:36:40 -0300

    Update list.service.ts

--- src/app/Service/list.service.ts
+++ src/app/Service/list.service.ts
@@ -2,3 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { list }  from '../Models/list.class';
 import { Observable } from 'rxjs';
+
+
+@Injectable()
+export class UserService {
+
+  constructor (private http: HttpClient ) { }
+
+}
